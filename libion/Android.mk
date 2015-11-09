c97df07035c132fff652c31566a8ec3f5f64fc30
David Marble <drmarble1@gmail.com>
2015-11-09 13:49:00 -0600

    Set ion build wno-error
    Needed for encore build
    
    Change-Id: Ide4bb52b0b8093e0b22cd98775b46fc05690e01e

--- libion/Android.mk
+++ libion/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include $(LOCAL_PATH)/kernel-headers
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include $(LOCAL_PATH)/kernel-headers
-LOCAL_CFLAGS := -Werror
+LOCAL_CFLAGS := -Wno-error
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -16,7 +16,7 @@ LOCAL_MODULE := iontest
 LOCAL_MODULE_TAGS := optional tests
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include $(LOCAL_PATH)/kernel-headers
 LOCAL_SHARED_LIBRARIES := liblog
-LOCAL_CFLAGS := -Werror
+LOCAL_CFLAGS := -Wno-error
 include $(BUILD_EXECUTABLE)
 
 include $(call first-makefiles-under,$(LOCAL_PATH))
